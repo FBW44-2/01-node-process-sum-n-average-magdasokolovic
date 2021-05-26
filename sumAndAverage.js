6b188417c0b2d2e08c154fc4a9acab9a82e82373
magdasokolovic <magda.sokolovic@gmail.com>
2021-05-26 14:14:49 +0200

    done

--- sumAndAverage.js
+++ sumAndAverage.js
@@ -1,6 +1,11 @@
 let arguments = process.argv.slice(2);
 let [operation, ...numbers] = arguments;
 
+for (let i=0; i<numbers.length; i++) {
+    if (isNaN(numbers[i])) {
+        return console.log(`Sorry, the argument ${numbers[i]} is not a number, please try again`)
+    }
+}
 if (operation === "sum") {
     let total = numbers.reduce((acc, number)=>{
         acc = acc + Number(number);
@@ -15,9 +20,7 @@ else if (operation === "avg") {
      }, +0)
      console.log("the average is: " + average)
 } 
-else if (numbers.filter(argument => typeof(argument) === "number")) {
-    console.log(`Sorry, the argument ${argument} is not a number, please try again`)
-} 
 else if (operation !== "sum" || "avg") {
     console.log(`I cannot calculate that, please type either "sum" (to calculate the sum) or "avg" (To calculate the Average)`)
 }
+
